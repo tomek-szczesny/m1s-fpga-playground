ae665c5ece1bba8878e9fc4c724ef0bc9dc0f392
Tomek Szczęsny <mctom@tlen.pl>
2023-12-22 19:26:17 +0100

    640x480 demo working, picture slightly off the screen

---
+++ gateware/vga/vga.py
@@ -0,0 +1,7 @@
+# This file contains clock constraints for the project.
+# Clock speeds are in MHz.
+
+ctx.addClock("EXTOSC", 100)
+ctx.addClock("clkvga", 25.5)
+ctx.addClock("fifo_clk_in", 25.5)
+ctx.addClock("fifo_clk_out", 25.5)
